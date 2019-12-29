bddddac12398cf71c76dbc07585bc4242f46cc25
Salman <isalman19@hotmail.com>
2019-12-30 00:53:16 +0100

    feat: add social media icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,12 @@ export default function About(props){
             <div className='container'>
                 <img src="/logo-sf.png" width='58px' alt="freeCodeCamp logo" />
                 <p>Resume &copy; Designed by Fahmi Salman</p>
+                <ul className='social-links'>
+                    <li><i className="fa fa-github" /></li>
+                    <li><i className="fa fa-linkedin" /></li>
+                    <li><i className="fa fa-facebook" /></li>
+                    <li><i className="fa fa-twitter" /></li>
+                </ul>
             </div>
         </section>
     );
