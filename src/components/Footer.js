5ddcafc3f269b15543e7784020c9e0d93e795bce
Salman <isalman19@hotmail.com>
2020-01-01 23:42:54 +0100

    fix: change footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function About(props){
     return(
-        <section className='footer' id='footer'>
+        <div className='footer' id='footer'>
             <div className='container'>
                 <img src="/logo-sf.png" width='58px' alt="freeCodeCamp logo" />
-                <p>Resume &copy; Designed by Fahmi Salman</p>
+                <div>Resume &copy; Designed by Fahmi Salman</div>
                 <ul className='social-links'>
                     <li><i className="fa fa-github" /></li>
                     <li><i className="fa fa-linkedin" /></li>
@@ -13,6 +13,6 @@ export default function About(props){
                     <li><i className="fa fa-twitter" /></li>
                 </ul>
             </div>
-        </section>
+        </div>
     );
 }
