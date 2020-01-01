7543597e499b1a1b8bb7e4d593e639f1e880801d
Salman <isalman19@hotmail.com>
2020-01-01 23:42:18 +0100

    feat: create experience tabs in progress

--- src/sections/Experiences.js
+++ src/sections/Experiences.js
@@ -5,9 +5,40 @@ export default function Experiences(props){
         <section className='Experiences' id='Experiences'>
             <div className='container'>
                 <h1 className='section-title'>Where I've Worked</h1>
-                <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-                </p>
+                <div className='row'>
+                    <div className='col-4'>
+                        <ul className='tablinks'>
+                            <li className='active'>Saham IT</li>
+                            <li>Altados</li>
+                            <li>Aerotechnic industries SA</li>
+                            <li>EBS Maroc</li>
+                            <li>DMS-Vision Group</li>
+                            <li>Info ++</li>
+                        </ul>
+                    </div>
+                    <div className='col-8'>
+                        <div className='tab-content'>
+                            <div className='tabpane'>
+                                Saham IT
+                            </div>
+                            <div className='tabpane'>
+                                Altados
+                            </div>
+                            <div className='tabpane'>
+                                EBS
+                            </div>
+                            <div className='tabpane'>
+                                Air France
+                            </div>
+                            <div className='tabpane'>
+                                DMS
+                            </div>
+                            <div className='tabpane'>
+                                Info ++
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
