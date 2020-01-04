12d0eba8bc80841b7f0364aeb8913aeded460ecc
Fahmi Salman <55559223+SalmanFahmi-IT@users.noreply.github.com>
2020-01-04 21:36:03 +0100

    Work/experience (#6)
    * feat: Home section in progress
    
    * feat: add scroll id to sections
    
    * feat: install font awesome icons package
    
    * style: add section title style and mobile nav
    
    * refactor: add content to sections
    
    * feat: add social media icons to footer
    
    * fix: import header component
    
    * fix: add images to static folder
    
    * feat: create experience tabs in progress
    
    * fix: change footer copyright
    
    * refactor: change components ext from js to jsx
    
    * style: add active class to tab link
    
    * feat: add experience data json
    
    * style: add transition style to tablink
    
    * feat: add tablink component
    
    * fix: update experience data
    
    * refactor: Display experience tabs using tablink
    
    * fix: update homepage container and fix exports
    
    * style: add style to experience description
    
    * fix: change tablink tag from li to button

--- src/sections/Experiences.js
+++ src/sections/Experiences.js
@@ -11,7 +11,7 @@ export default function Experiences(props){
                 <h1 className='section-title'>Where I've Worked</h1>
                 <div className='row'>
                     <div className='col-4'>
-                        <ul className='tablinks'>
+                        <div className='tablinks'>
                             {
                                 ExperiencesData.map((exp, index) => {
                                     return <TabLink 
@@ -23,16 +23,25 @@ export default function Experiences(props){
                                     />
                                 })
                             }
-                        </ul>
+                        </div>
                     </div>
                     <div className='col-8'>
                         <div className='tab-content'>
                            <div className='tabpane'>
-                                <h4>{ExperiencesData[currentTab].title} @ {ExperiencesData[currentTab].company}</h4>
-                                <h6>{ExperiencesData[currentTab].date}</h6>
-                                <p>{ExperiencesData[currentTab].description}</p>
-                                <p><strong>Method:</strong> {ExperiencesData[currentTab].method}</p>
-                                <p><strong>Skills:</strong> {ExperiencesData[currentTab].skills}</p>
+                                <h4>{ExperiencesData[currentTab].title} @ 
+                                    <span className='theme-color'>
+                                        {ExperiencesData[currentTab].company} 
+                                    </span>
+                                </h4> 
+                                <p className='theme-color lighter'>
+                                    {ExperiencesData[currentTab].date}
+                                </p>
+                                <div className='desc'>
+                                    <h5>Description :</h5>
+                                    <p>{ExperiencesData[currentTab].description}</p>
+                                </div>
+                                <h5>Skills :</h5>
+                                <p>{ExperiencesData[currentTab].skills}</p>
                             </div>
                         </div>
                     </div>
