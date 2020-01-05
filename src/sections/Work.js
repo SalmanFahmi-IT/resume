7af27ff2edb1665a154bfeafde66ba441cbdc04e
Fahmi Salman <55559223+SalmanFahmi-IT@users.noreply.github.com>
2020-01-05 03:14:43 +0100

    Work/projects (#7)
    * feat: add projects to work component
    
    * refactor: using space between paragraphs
    
    * style: add project style css
    
    * feat: add contact form to contact component and personal infos

--- src/sections/Work.js
+++ src/sections/Work.js
@@ -5,9 +5,150 @@ export default function Work(props){
         <section className='work' id='work'>
             <div className='container'>
                 <h1 className='section-title'>Some Things I've Built</h1>
-                <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-                </p>
+                <div className="row featured">
+                    <div className="col-6">
+                        <img src="/screenshot.png" alt='Salman' className="screenshot"/>
+                    </div>
+                    <div className="col-6">
+                        <div className='right'>
+                            <span className='theme-color lighter'>
+                                Featured project
+                            </span>
+                            <h4>Orange Sales App</h4>
+                        </div>
+                        <p>
+                        Nisi non laboris magna adipisicing aute enim fugiat velit tempor sunt voluptate mollit. Id exercitation sunt tempor cupidatat ipsum. Qui do veniam anim veniam eiusmod eu incididunt ex officia ea ipsum. Culpa ullamco elit cupidatat nulla consectetur. Laborum laboris laboris amet esse tempor quis quis.
+                        </p>
+                        <ul className='project-skills'>
+                            <li>SCSS</li>
+                            <li>ReactJs</li>
+                            <li>Redux</li>
+                            <li>Redux Thunk</li>
+                            <li>NodeJs</li>
+                        </ul>
+                    </div>
+                    
+                </div>
+                
+                <div className="row">
+                    <div className='col-12 center'>
+                    <h4>Other Noteworthy Projects</h4>
+                    <span className='theme-color lighter'>view the archive</span>
+                    </div>
+                    <div className="col-4">
+                        <div className='work-box'>
+                            <ul className='box-links'>
+                                <li><a href='/#'><i className='fa fa-github' /></a></li>
+                                <li><a href='/#'><i className='fa fa-external-link' /></a></li>
+                            </ul>
+                            <h4>Project name </h4> 
+                            <span className='theme-color lighter'>
+                                Personal
+                            </span>
+                            <p>
+                                Lorem  Pariatur nostrud reprehenderit occaecat est exercitation nostrud elit. Laboris adipisicing excepteur dolor quis laborum velit do velit culpa officia incididunt dolor magna.
+                            </p>
+                            <p>
+                                ReactJs - Redux - Thunk - NodeJs
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className='work-box'>
+                            <ul className='box-links'>
+                                <li><a href='/#'><i className='fa fa-github' /></a></li>
+                                <li><a href='/#'><i className='fa fa-external-link' /></a></li>
+                            </ul>
+                            <h4>Project name </h4> 
+                            <span className='theme-color lighter'>
+                                Personal
+                            </span>
+                            <p>
+                                Lorem  Pariatur nostrud reprehenderit occaecat est exercitation nostrud elit. Laboris adipisicing excepteur dolor quis laborum velit do velit culpa officia incididunt dolor magna.
+                            </p>
+                            <p>
+                                ReactJs - Redux - Thunk - NodeJs
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className='work-box'>
+                            <ul className='box-links'>
+                                <li><a href='/#'><i className='fa fa-github' /></a></li>
+                                <li><a href='/#'><i className='fa fa-external-link' /></a></li>
+                            </ul>
+                            <h4>Project name </h4> 
+                            <span className='theme-color lighter'>
+                                Company
+                            </span>
+                            <p>
+                                Lorem  Pariatur nostrud reprehenderit occaecat est exercitation nostrud elit. Laboris adipisicing excepteur dolor quis laborum velit do velit culpa officia incididunt dolor magna.
+                            </p>
+                            <p>
+                                ReactJs - Redux - Thunk - NodeJs
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className='work-box'>
+                            <ul className='box-links'>
+                                <li><a href='/#'><i className='fa fa-github' /></a></li>
+                                <li><a href='/#'><i className='fa fa-external-link' /></a></li>
+                            </ul>
+                            <h4>Project name </h4> 
+                            <span className='theme-color lighter'>
+                                Company
+                            </span>
+                            <p>
+                                Lorem  Pariatur nostrud reprehenderit occaecat est exercitation nostrud elit. Laboris adipisicing excepteur dolor quis laborum velit do velit culpa officia incididunt dolor magna.
+                            </p>
+                            <p>
+                                ReactJs - Redux - Thunk - NodeJs
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className='work-box'>
+                            <ul className='box-links'>
+                                <li><a href='/#'><i className='fa fa-github' /></a></li>
+                                <li><a href='/#'><i className='fa fa-external-link' /></a></li>
+                            </ul>
+                            <h4>Project name </h4> 
+                            <span className='theme-color lighter'>
+                                Company
+                            </span>
+                            <p>
+                                Lorem  Pariatur nostrud reprehenderit occaecat est exercitation nostrud elit. Laboris adipisicing excepteur dolor quis laborum velit do velit culpa officia incididunt dolor magna.
+                            </p>
+                            <p>
+                                ReactJs - Redux - Thunk - NodeJs
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className='work-box'>
+                            <ul className='box-links'>
+                                <li><a href='/#'><i className='fa fa-github' /></a></li>
+                                <li><a href='/#'><i className='fa fa-external-link' /></a></li>
+                            </ul>
+                            <h4>Project name </h4> 
+                            <span className='theme-color lighter'>
+                                Company
+                            </span>
+                            <p>
+                                Lorem  Pariatur nostrud reprehenderit occaecat est exercitation nostrud elit. Laboris adipisicing excepteur dolor quis laborum velit do velit culpa officia incididunt dolor magna.
+                            </p>
+                            <p>
+                                ReactJs - Redux - Thunk - NodeJs
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12 center'>
+                        <button className='btn'>View all projects</button>
+                    </div>
+                </div>
             </div>
         </section>
     );
