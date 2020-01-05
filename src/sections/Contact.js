121e753a569728acb272eb68bdce0e3b8a5b5271
Salman <isalman19@hotmail.com>
2020-01-05 03:11:10 +0100

    feat: add contact form to contact component and personal infos

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -5,9 +5,25 @@ export default function Contact(props){
         <section className='contact' id='contact'>
             <div className='container'>
                 <h1 className='section-title'>Contact</h1>
-                <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-                </p>
+                <div className='row'>
+                    <div className='col-4'>
+                        <ul className='lighter'>
+                            <li><i className='fa fa-envelope' /> fahmi.salmn@gmail.com</li>
+                            <li><i className='fa fa-phone' /> fahmi.salmn@gmail.com</li>
+                            <li><i className='fa fa-map' /> fahmi.salmn@gmail.com</li>
+                            <li><i className='fa fa-link' /> fahmi.salmn@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div className='col-8'>
+                        <form>
+                            <input type='text' placeholder='Email'/>
+                            <input type='text' placeholder='Phone'/>
+                            <input type='text' placeholder='Website'/>
+                            <textarea rows='5' placeholder='Message'></textarea>
+                            <button type='submit' className='btn'>Send message</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </section>
     );
